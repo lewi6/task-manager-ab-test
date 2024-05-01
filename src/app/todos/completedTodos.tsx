73f410fe2779c55a6598dc6be43b4599976ed854
lewi6 <lewisizere@Lewiss-MacBook-Air.local>
2024-05-01 21:44:08 +0200

    tanstack and ci/cd setup

---
+++ src/app/todos/completedTodos.tsx
@@ -0,0 +1,67 @@
+"use client";
+
+import { useTodoStore } from "@/components/Store/DummyTodo";
+import { useQuery } from "@tanstack/react-query";
+import { useEffect, useState } from "react";
+
+export default function TodoList() {
+  const { data, isLoading, isFetching } = useQuery({
+    queryKey: ["todos"],
+    queryFn: getTodos,
+  });
+
+  const [showCompleted, setShowCompleted] = useState(false);
+
+  console.log(data, isLoading, isFetching);
+  const { completedTodos, setCompletedTodos } = useTodoStore();
+
+  //   useEffect(() => {
+  //     if (isFetching || isLoading) return;
+  //     setTodos(data?.todos);
+  //   }, [data?.todos, isFetching, isLoading, setTodos]);
+
+  if (isFetching) return <>loading...</>;
+  return (
+    <div className="flex flex-col w-[650px] mx-auto gap-2">
+      <h1 className="cursor-pinter p-2 border-red-800"> ALL</h1>
+
+      <div className="flex flex-row w-full justify-between">
+        <button
+          className="flex bg-blue"
+          onClick={() => setShowCompleted(false)}
+        >
+          ALL
+        </button>
+
+        <button
+          className=" flex bg-green"
+          onClick={() => setShowCompleted(true)}
+        >
+          Completed
+        </button>
+      </div>
+      {data?.todos
+        .filter(completedTodos.includes(data?.todos.id))
+        ?.map((obj: any, i: number) => (
+          <div className="flex flex-row justify-between" key={i}>
+            <div>{obj.id}</div>
+            <div>{obj.todo}</div>
+            <div>{obj.completed ? "true" : "false"}</div>
+            <div>{obj.userId}</div>
+
+            <input
+              type="checkbox"
+              checked={completedTodos.includes(obj.id)}
+              onChange={(e) => setCompletedTodos([...completedTodos, obj.id])}
+            />
+          </div>
+        ))}
+    </div>
+  );
+}
+
+const getTodos = async () => {
+  const res = await fetch(`https://dummyjson.com/todos`);
+
+  return res.json();
+};
---
+++ src/app/todos/todo.tsx
@@ -0,0 +1,93 @@
+"use client";
+
+import { useQuery } from "@tanstack/react-query";
+import { useEffect, useState } from "react";
+import _ from "lodash";
+import { useTodoStore } from "@/components/Store/DummyTodo";
+export default function TodoList() {
+  const { data, isLoading, isFetching } = useQuery({
+    queryKey: ["todos"],
+    queryFn: getTodos,
+  });
+
+  const [showCompleted, setShowCompleted] = useState(false);
+
+  console.log(data, isLoading, isFetching);
+  const { completedTodos, setCompletedTodos } = useTodoStore();
+
+  //   useEffect(() => {
+  //     if (isFetching || isLoading) return;
+  //     setTodos(data?.todos);
+  //   }, [data?.todos, isFetching, isLoading, setTodos]);
+
+  if (isFetching) return <>loading...</>;
+  return (
+    <div className="flex flex-col w-[650px] mx-auto gap-2">
+      <div className="flex flex-row w-full justify-between">
+        <button
+          className="flex bg-blue"
+          onClick={() => setShowCompleted(false)}
+        >
+          ALL
+        </button>
+
+        <button
+          className=" flex bg-green"
+          onClick={() => setShowCompleted(true)}
+        >
+          Completed
+        </button>
+      </div>
+      {showCompleted &&
+        data?.todos
+          .filter((obj: any) => completedTodos.includes(obj.id))
+          ?.map((obj: any, i: number) => (
+            <div className="flex flex-row justify-between" key={i}>
+              <div>{obj.id}</div>
+              <div>{obj.todo}</div>
+              <div>{obj.completed ? "true" : "false"}</div>
+              <div>{obj.userId}</div>
+
+              <input
+                type="checkbox"
+                checked={completedTodos.includes(obj.id)}
+                onClick={(e) =>
+                  completedTodos.includes(obj.id)
+                    ? setCompletedTodos([
+                        ...completedTodos.filter((id) => obj.id !== id),
+                      ])
+                    : setCompletedTodos([...completedTodos, obj.id])
+                }
+              />
+            </div>
+          ))}
+      {!showCompleted &&
+        data?.todos?.map((obj: any, i: number) => (
+          <div className="flex flex-row justify-between" key={i}>
+            <div>{obj.id}</div>
+            <div>{obj.todo}</div>
+            <div>{obj.completed ? "true" : "false"}</div>
+            <div>{obj.userId}</div>
+
+            <input
+              type="checkbox"
+              checked={completedTodos.includes(obj.id)}
+              onClick={(e) =>
+                completedTodos.includes(obj.id)
+                  ? setCompletedTodos([
+                      ...completedTodos.filter((id) => obj.id !== id),
+                    ])
+                  : setCompletedTodos([...completedTodos, obj.id])
+              }
+            />
+          </div>
+        ))}
+    </div>
+  );
+}
+
+const getTodos = async () => {
+  const res = await fetch(`https://dummyjson.com/todos?limit=10`);
+
+  return res.json();
+};
